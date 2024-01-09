bc36b2eced58804d1ecb973389df46e6653ded21
Ali Iqbal <ali.iqbal3578@gmail.com>
2024-01-09 16:34:51 -0600

    arrays done

---
+++ 19-arrays/2-arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int age[5];
+    
+    age[0] = 21;
+    age[1] = 29;
+    age[2] = 25;
+    age[3] = 32;
+    age[4] = 17;
+
+    printf("%d ", age[0]);
+    printf("%d ", age[1]);
+    printf("%d ", age[2]);
+    printf("%d ", age[3]);
+    printf("%d ", age[4]);
+    
+    return 0;
+}
